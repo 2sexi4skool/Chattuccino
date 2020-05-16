fc85b269190138dc99386735af231cb4c7a0b27c
Francis Powlesland <francis.powlesland1@ibm.com>
2020-05-16 20:40:14 +0100

    Initial messages view complete

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
+  <v-app class="d-flex flex-column">
+    <v-app-bar dense app color="primary" dark flat>
       <div class="d-flex align-center">
         <h1 style="font-weight: 400;">Chattuccino</h1>
       </div>
       <v-spacer></v-spacer>
     </v-app-bar>
 
-    <v-content></v-content>
-    <div class="align-end">
-      <v-container m-2>
-        <v-text-field append-outer-icon="mdi-send"></v-text-field>
-      </v-container>
+    <v-content id="messagesBox" class="mx-4 overflow-y-auto"  style="height: 100px;">
+      <p v-for="(message) in messages" :key="message.id">{{ message.msg }}</p>
+    </v-content>
+    <div class="align-end mr-4 extendMarginLeft">
+      <v-text-field
+      hide-details
+        v-model="draftMessage"
+        v-on:keydown.enter="sendMessage"
+        append-outer-icon="mdi-send"
+      ></v-text-field>
     </div>
   </v-app>
 </template>
@@ -23,7 +28,27 @@ export default {
   components: {},
 
   data: () => ({
-    //
-  })
+    draftMessage: "",
+    messages: []
+    }),
+  methods: {
+    sendMessage() {
+      console.log("message received", this.draftMessage);
+      const message = {msg: this.draftMessage, id: Date.now(), sentByMe: true}
+      this.messages.push(message);
+      this.draftMessage = "";
+    }
+  }
 };
 </script>
+
+<style>
+.extendMarginLeft {
+  margin-left: 3.0625rem;
+  margin-bottom: 0.5rem;
+}
+
+#messagesBox::-webkit-scrollbar {
+  display: none;
+}
+</style>
