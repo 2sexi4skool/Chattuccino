aa9fe64ac43311e1a37bc95a89643fbe9e23586e
Francis Powlesland <francis.powlesland1@ibm.com>
2020-05-17 18:50:11 +0100

    socket messages now re-emitted

--- applySockets.js
+++ applySockets.js
@@ -1,8 +1,22 @@
 exports.init = (io) => {
  
     const nsp = io.of('/chat');
-nsp.on('connection', function(socket){
+    nsp.on('connection', function (socket) {
+    
+        socket.on('hb', (data) => {
+            console.log('heartbeat', data)
+            socket.emit('hb')
+        })
+
     console.log('someone connected');
-    socket.on('selectRoom', (room) => socket.join(room))
+        socket.on('selectRoom', (room) => {
+            console.log('someone joined room', room)
+            socket.join(room)
+    })
+
+        socket.on('chatMessage', (data) => {
+        const {room} = data
+        nsp.to(room).emit('chatMessage', data)
+    })
 });
 }
