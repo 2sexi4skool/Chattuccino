20b0798b12797d383ae7a6f8f696f3aa64f1bd63
Francis Powlesland <francis.powlesland1@ibm.com>
2020-05-17 11:55:18 +0100

    Speech bubbles now contain timestamps

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <v-app class="d-flex flex-column">
-    <v-app-bar dense app color="primary" dark flat>
+    <v-app-bar dense app color="#071E22" dark flat>
       <div class="d-flex align-center">
         <h1 style="font-weight: 400;">Chattuccino</h1>
       </div>
       <v-spacer></v-spacer>
     </v-app-bar>
 
-    <v-content id="messagesBox" class="mx-4 overflow-y-auto"  style="height: 100px;">
-      <p v-for="(message) in messages" :key="message.id">{{ message.msg }}</p>
+    <v-content class="mx-4" style="height: 100px;">
+      <div ref="messagesBox" class="overflow-y-auto px-8" style="height: 100%;">
+        <div class="d-flex flex-column">
+          <div style="margin-top: 1rem" />
+          <speech-bubble
+            v-for="message in messages"
+            :key="message.timestamp"
+            :message="message.body"
+            :class="{
+              myMessage: message.sentByMe,
+              otherMessage: !message.sentByMe,
+              baseMessage: true
+            }"
+            :arrowDirection="message.sentByMe ? 'right' : 'left'"
+            :color="message.sentByMe ? '#DEA47E' : '#F8F2DC'"
+            :suffix="message.timestamp | format('E HH:mm')"
+          />
+        </div>
+      </div>
     </v-content>
     <div class="align-end mr-4 extendMarginLeft">
       <v-text-field
-      hide-details
+        auto-grow
+        hide-details
         v-model="draftMessage"
         v-on:keydown.enter="sendMessage"
         append-outer-icon="mdi-send"
@@ -22,29 +40,61 @@
 </template>
 
 <script>
+import { format } from "date-fns";
+import SpeechBubble from "@/components/SpeechBubble.vue";
 export default {
   name: "App",
 
-  components: {},
+  components: {
+    SpeechBubble
+  },
 
   data: () => ({
     draftMessage: "",
-    messages: []
-    }),
+    messages: [
+    ]
+  }),
   methods: {
     sendMessage() {
-      const message = {msg: this.draftMessage, id: Date.now(), sentByMe: true}
+      if (!this.draftMessage.replace(/\s/g, "").length) {
+        return;
+      }
+      const message = {
+        body: this.draftMessage,
+        timestamp: Date.now(),
+        sentByMe: true
+      };
       this.messages.push(message);
       this.draftMessage = "";
+      this.$nextTick(() =>
+        this.$refs.messagesBox.scroll(0, this.$refs.messagesBox.scrollHeight)
+      );
     }
+  },
+  filters: {
+    format
   }
 };
 </script>
 
 <style>
+
+/* https://coolors.co/F8F2DC-dea47e-071e22-f8f2dc-1d7874 */
 .extendMarginLeft {
   margin-left: 1rem;
   margin-bottom: 0.5rem;
+}
+
+.myMessage {
+  align-self: flex-end;
+}
+
+.otherMessage {
+  align-self: flex-start;
+}
+
+.baseMessage {
+  max-width: 65%;
 }
 
 #messagesBox::-webkit-scrollbar {
---
+++ frontend/src/components/SpeechBubble.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    :class="`speechbubble ${arrowDirection}`"
+    :style="{ backgroundColor: color }"
+  >
+    <div class="messageContent">{{ message }}</div>
+    <div :style="suffixPosition">{{suffix}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["message", "arrowDirection", "color", "suffix"],
+  computed: {
+    suffixPosition() {
+      return {
+        float: this.arrowDirection,
+        fontSize: '.7rem',
+        color: '#071E22DE',
+        padding: '0 .5rem'
+        
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.messageContent {
+  padding: .5rem;
+  overflow-wrap: break-word;
+  color: #071E22;
+}
+
+.speechbubble {
+  position: relative;
+  border-radius: 0.4rem;
+  min-height: 2.5rem;
+  margin-bottom: 0.5rem;
+}
+
+.left {
+}
+.left::after {
+  content: "";
+  position: absolute;
+  width: 10px;
+  height: 20px;
+  top: 40%;
+  left: -0.6rem;
+  background-color: #f8f2dc;
+  clip-path: polygon(100% 0, 0 50%, 100% 100%);
+}
+
+.right {
+}
+
+.right::after {
+  content: "";
+  position: absolute;
+  width: 10px;
+  height: 20px;
+  top: 40%;
+  right: -0.6rem;
+  background-color: #dea47e;
+  clip-path: polygon(0 0, 100% 50%, 0 100%);
+}
+</style>
